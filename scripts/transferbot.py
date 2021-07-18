14cf7ba847c3b701d0dd1f2485a99402f654478e
Skh sourav halder <54745263+Skhsouravhalder@users.noreply.github.com>
2021-07-19 02:02:37 +0600

    Update transferbot.py

--- scripts/transferbot.py
+++ scripts/transferbot.py
@@ -166,7 +166,7 @@ def main(*args) -> None:
         source_link = page.title(as_link=True, insite=targetpage.site)
 
 
-        targetpage.put(text, summary=NewPages and other)
+        targetpage.put(text, summary="add")
 
 
 if __name__ == '__main__':
